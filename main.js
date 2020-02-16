c8c4dd7962d0943633fd1ccc5a24377d07f4879e
Frithjof Winkelmann <frithjof97@web.de>
2020-02-16 11:54:10 +0100

    Make it prettier!

--- main.js
+++ main.js
@@ -3,5 +3,24 @@ function generateSVG() {
 
     var img = document.getElementById('img');
 
-    img.src = 'https://detext.haskai.de/tex2svg/?latexB64=' + btoa(latex);
+    var url = 'https://detext.haskai.de/tex2svg/?latexB64=' + btoa(latex);
+    var urlLink = url + '&scale=5';
+
+    img.src = url;
+
+    document.getElementById('img-href').setAttribute('href', urlLink);
+
+    var urlText = document.getElementById('url');
+    urlText.innerText = url;
+    document.getElementById('result').setAttribute('style', 'visibility: visible');
+}
+
+function copyToClipBoard() {
+    var latex = document.getElementById('latex').value;
+
+    var url = 'https://detext.haskai.de/tex2svg/?latexB64=' + btoa(latex) + '&scale=5';
+
+    navigator.clipboard.writeText(url);
+
+    alert('Copied to clipbaord');
 }
